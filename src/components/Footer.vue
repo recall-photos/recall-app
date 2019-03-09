c1b170ad368ed548c0068efe8b22784e9f0e42f0
Jack Veiga <hello@jackveiga.com>
2019-03-09 19:20:49 +0800

    Separate footer to a component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,67 @@
 <template>
-  <div>
-    <div class="mt5 mt6-ns mb4 ph5 center">
+  <div class="mt-32 pt-24 pb-16 bg-purple-darkest p-8">
+    <div class="sm:flex mb-4">
+      <div class="sm:w-1/4 h-auto">
+        <div class="text-orange mb-2">Orange</div>
+        <ul class="list-reset leading-normal">
+          <li class="hover:text-orange text-grey-darker">One</li>
+          <li class="hover:text-orange text-grey-darker">Two</li>
+          <li class="hover:text-orange text-grey-darker">Three</li>
+          <li class="hover:text-orange text-grey-darker">Four</li>
+          <li class="hover:text-orange text-grey-darker">Five</li>
+          <li class="hover:text-orange text-grey-darker">Six</li>
+          <li class="hover:text-orange text-grey-darker">Seven</li>
+          <li class="hover:text-orange text-grey-darker">Eight</li>
+        </ul>
+      </div>
+      <div class="sm:w-1/4 h-auto sm:mt-0 mt-8">
+        <div class="text-blue mb-2">Blue</div>
+        <ul class="list-reset leading-normal">
+          <li class="hover:text-blue text-grey-darker">One</li>
+          <li class="hover:text-blue text-grey-darker">Two</li>
+          <li class="hover:text-blue text-grey-darker">Three</li>
+        </ul>
+
+        <div class="text-blue-light mb-2 mt-4">Blue-light</div>
+        <ul class="list-reset leading-normal">
+          <li class="hover:text-blue-light text-grey-darker">One</l>i
+          <li class="hover:text-blue-light text-grey-darker">Two</li>
+          <li class="hover:text-blue-light text-grey-darker">Three</li>
+        </ul>
+      </div>
+      <div class="sm:w-1/4 h-auto sm:mt-0 mt-8">
+        <div class="text-green-dark mb-2">Green-dark</div>
+        <ul class="list-reset leading-normal">
+          <li class="hover:text-green-dark text-grey-darker">One</li>
+          <li class="hover:text-green-dark text-grey-darker">Two</li>
+          <li class="hover:text-green-dark text-grey-darker">Three</li>
+        </ul>
+
+        <div class="text-green-light mb-2 mt-4">Green-light</div>
+        <ul class="list-reset leading-normal">
+          <li class="hover:text-green-light text-grey-darker">One</li>
+          <li class="hover:text-green-light text-grey-darker">Two</li>
+          <li class="hover:text-green-light text-grey-darker">Three</li>
+        </ul>
+      </div>
+      <div class="sm:w-1/2 sm:mt-0 mt-8 h-auto">
+        <div class="text-red-light mb-2">Newsletter</div>
+        <p
+          class="text-grey-darker leading-normal"
+        >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, consectetur.</p>
+        <div class="mt-4 flex">
+          <input
+            type="text"
+            class="p-2 border border-grey-light round text-grey-dark text-sm h-auto"
+            placeholder="Your email address"
+          >
+          <button class="bg-orange text-white rounded-sm h-auto text-xs p-3">Subscribe</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="mt5 mt6-ns mb4 ph5 center">
       <a
         class="link o-80 near-black hover-purple dib h2 w2 mr3"
         href="https://github.com/recall-photos/recall-app"
@@ -67,6 +128,5 @@
           ></path>
         </svg>
       </a>
-    </div>
-  </div>
+  </div>-->
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,115 +1,131 @@
 <template>
-  <div class="container mx-auto lg: px-4">
-    <div class="my-10 flex justify-between">
-      <div class>
-        <img alt="Recall Logo" class="w-2/3 cursor-pointer" src="../assets/svg/recall-symbol.svg">
+  <div>
+    <div class="container mx-auto lg: px-4">
+      <div class="my-10 flex justify-between">
+        <div class>
+          <img alt="Recall Logo" class="w-2/3 cursor-pointer" src="../assets/svg/recall-symbol.svg">
+        </div>
+        <div
+          class="inline-flex flex-row flex-no-wrap items-center self-center justify-around cursor-pointer text-sm font-mono"
+        >
+          <a
+            href="https://recall.photos"
+            class="mr-8 text-black no-underline hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Features</a>
+          <a
+            href="https://recall.photos"
+            class="mr-8 text-black no-underline hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >How to get started</a>
+          <a
+            href="https://recall.photos"
+            class="mr-8 text-black no-underline hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Share our mission</a>
+          <a
+            href="https://recall.photos"
+            class="btn font-bold"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Sign in</a>
+        </div>
       </div>
-      <div
-        class="inline-flex flex-row flex-no-wrap items-center self-center justify-around cursor-pointer text-sm font-mono"
-      >
-        <a
-          href="https://recall.photos"
-          class="mr-8 text-black no-underline hover:underline"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Features</a>
-        <a
-          href="https://recall.photos"
-          class="mr-8 text-black no-underline hover:underline"
-          target="_blank"
-          rel="noopener noreferrer"
-        >How to get started</a>
-        <a
-          href="https://recall.photos"
-          class="mr-8 text-black no-underline hover:underline"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Share our mission</a>
-        <a
-          href="https://recall.photos"
-          class="btn font-bold"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Sign in</a>
-      </div>
-    </div>
-    <div class="mt-24">
-      <div class="flex flex-row flex-wrap items-center content-center self-center justify-between">
-        <div class="mt-24 w-1/2 pr-10 flex flex-col items-start content-start self-start">
-          <h1 class="text-left text-black font-sans text-5xl font-bold">Recall
-            <vue-typer
-              :text='["happy memories","family trips","old friendships","loved ones", "your stories","special moments", "past adventures", "holidays abroad"]'
-              :repeat="Infinity"
-              :shuffle="true"
-              initial-action="typing"
-              :pre-type-delay="70"
-              :type-delay="70"
-              :pre-erase-delay="2000"
-              :erase-delay="250"
-              erase-style="backspace"
-              :erase-on-complete="false"
-              caret-animation="smooth"
-            ></vue-typer>
-          </h1>
-          <h2
-            class="mt-12 text-left text-black font-sans text-2xl font-semi leading-tight"
-          >The most secure, free, end-to-end encrypted and open-source alternative to Google Photos</h2>
-          <p
-            class="mt-4 text-left text-black font-sans text-base font-normal leading-normal opacity-75"
-          >Your new personal photo vault. We believe that photos are a true reflection of your life so you’ll want to keep them safe and private, storing them without any restrictions while retaining full ownership and privacy.</p>
-          <div class="mt-12">
-            <div v-if="authenticated">Logged in</div>
-            <div v-else class="flex flex-col items-start content-start self-stretch">
-              <button @click="login" class="btn">
-                <img alt="Blockstack Logo" class="w-8 ml-1" src="../assets/svg/blockstack.svg">
-                <span class="font-mono font-bold text-sm ml-4 pr-2">Start using it with Blockstack</span>
-              </button>
-              <button
-                @click="window.open(
+      <div class="mt-24">
+        <div
+          class="flex flex-row flex-wrap items-center content-center self-center justify-between"
+        >
+          <div class="mt-24 w-1/2 pr-10 flex flex-col items-start content-start self-start">
+            <h1 class="text-left text-black font-sans text-5xl font-bold">Recall
+              <vue-typer
+                :text='["happy memories","family trips","old friendships","loved ones", "your stories","special moments", "past adventures", "holidays abroad"]'
+                :repeat="Infinity"
+                :shuffle="true"
+                initial-action="typing"
+                :pre-type-delay="70"
+                :type-delay="70"
+                :pre-erase-delay="2000"
+                :erase-delay="250"
+                erase-style="backspace"
+                :erase-on-complete="false"
+                caret-animation="smooth"
+              ></vue-typer>
+            </h1>
+            <h2
+              class="mt-12 text-left text-black font-sans text-2xl font-semi leading-tight"
+            >The most secure, free, end-to-end encrypted and open-source alternative to Google Photos</h2>
+            <p
+              class="mt-4 text-left text-black font-sans text-base font-normal leading-normal opacity-75"
+            >Your new personal photo vault. We believe that photos are a true reflection of your life so you’ll want to keep them safe and private, storing them without any restrictions while retaining full ownership and privacy.</p>
+            <div class="mt-12">
+              <div v-if="authenticated">Logged in</div>
+              <div v-else class="flex flex-col items-start content-start self-stretch">
+                <button @click="login" class="btn">
+                  <img alt="Blockstack Logo" class="w-8 ml-1" src="../assets/svg/blockstack.svg">
+                  <span class="font-mono font-bold text-sm ml-4 pr-2">Start using it with Blockstack</span>
+                </button>
+                <button
+                  @click="window.open(
                   'https://testflight.apple.com/join/YuXc2asc',
                   '_blank'
                 );"
-                class="mt-4 btn-alt"
-              >
-                <img alt="iOS App Store Logo" class="w-8 ml-1" src="../assets/svg/apple.svg">
-                <span
-                  class="font-mono font-bold text-sm ml-4 pr-2 text-grey-dark"
-                >Help us beta test our iOS app</span>
-              </button>
+                  class="mt-4 btn-alt"
+                >
+                  <img alt="iOS App Store Logo" class="w-8 ml-1" src="../assets/svg/apple.svg">
+                  <span
+                    class="font-mono font-bold text-sm ml-4 pr-2 text-grey-dark"
+                  >Help us beta test our iOS app</span>
+                </button>
+              </div>
             </div>
           </div>
+          <img
+            class="w-1/2 rounded"
+            alt="Recall illustration by "
+            src="../assets/img/recall-opening.jpg"
+          >
         </div>
-        <img
-          class="w-1/2 rounded"
-          alt="Recall illustration by "
-          src="../assets/img/recall-opening.jpg"
+      </div>
+      <div class="pt-32 mt-24">
+        <div class="flex flex-col items-start">
+          <h2
+            class="text-black font-sans text-4xl font-semi leading-tight"
+          >Why you should use Recall</h2>
+        </div>
+        <div class="mt-16">
+          <Icon v-bind:iconItems="iconItems"></Icon>
+        </div>
+      </div>
+      <div class="mt-32 pt-24">
+        <h2
+          class="text-left text-black font-sans text-4xl font-semi leading-tight"
+        >How to get started</h2>
+        <div
+          class="flex flex-row flex-wrap items-center content-center self-center justify-between"
         >
-      </div>
-    </div>
-    <div class="pt-32 mt-24">
-      <div class="flex flex-col items-start">
-        <h2 class="text-black font-sans text-2xl font-semi leading-tight">Why you should use Recall</h2>
-      </div>
-      <div class="mt-12">
-        <Icon v-bind:iconItems="iconItems"></Icon>
-      </div>
-    </div>
-    <div class="mt-32">
-      <h2
-        class="float-left text-black font-sans text-2xl font-semi leading-tight"
-      >How to get started</h2>
-      <div class="flex flex-row justify-between">
-        <div class="w-1/2"></div>
-        <div class="flex flex-col items-start w-1/2">
-          <SmallCard v-bind:SmallCardItems="SmallCardItems"></SmallCard>
+          <div class="w-1/2">
+            <img
+              class="w-4/5 mt-32 rounded"
+              alt="Recall illustration by "
+              src="../assets/img/recall-how.jpg"
+            >
+          </div>
+          <div class="flex flex-col items-center w-1/2">
+            <SmallCard v-bind:SmallCardItems="SmallCardItems"></SmallCard>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="pt-32 mt-24">
-      <div class="flex flex-col items-start">
-        <h2 class="text-black font-sans text-2xl font-semi leading-tight">Help us share our mission</h2>
-        <div class="flex flex-row justify-around items-center">
-          <TwitterCard v-bind:TwitterCardItems="TwitterCardItems"></TwitterCard>
+      <div class="mt-32 pt-24">
+        <div class="flex flex-col items-start">
+          <h2
+            class="text-black font-sans text-4xl font-semi leading-tight"
+          >Help us share our mission</h2>
+          <div class="flex flex-row justify-around items-center">
+            <TwitterCard v-bind:TwitterCardItems="TwitterCardItems"></TwitterCard>
+          </div>
         </div>
       </div>
     </div>
@@ -186,24 +202,24 @@ export default {
       SmallCardItems: [
         {
           card_id: 1,
-          card_svg: require("../assets/svg/features/private.svg"),
+          card_img: require("../assets/img/6.jpg"),
           card_title: "Create an account with Blockstack",
           card_paragraph:
-            "When you start using Recall, first you will have to create your account, by generating a secret key and a password."
+            "To start using Recall, first you have to create an account with Blockstack. You are routed automatically from our app. This will generate a secret key and a password."
         },
         {
           card_id: 2,
-          card_svg: require("../assets/svg/features/private.svg"),
+          card_img: require("../assets/img/7.jpg"),
           card_title: "Authorise Recall to access your ID",
           card_paragraph:
-            "When you start using Recall, first you will have to create your account, by generating a secret key and a password."
+            "To start using Recall, first you have to create an account with Blockstack. You are routed automatically from our app. This will generate a secret key and a password."
         },
         {
           card_id: 3,
-          card_svg: require("../assets/svg/features/private.svg"),
+          card_img: require("../assets/img/8.jpg"),
           card_title: "Upload your images onto Recall",
           card_paragraph:
-            "When you start using Recall, first you will have to create your account, by generating a secret key and a password."
+            "To start using Recall, first you have to create an account with Blockstack. You are routed automatically from our app. This will generate a secret key and a password."
         }
       ],
       TwitterCardItems: [
