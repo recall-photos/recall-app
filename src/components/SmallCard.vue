0f4f0120030f056c5d391dc15bb91fb2ad2b82bb
Jack Veiga <hello@jackveiga.com>
2019-03-11 22:32:48 +0800

    Changes to small cards for how to get started

--- src/components/SmallCard.vue
+++ src/components/SmallCard.vue
@@ -3,17 +3,14 @@
     <div
       v-for="item in SmallCardItems"
       :key="item.card_id"
-      class="mt-8 max-w-sm w-full flex flex-row p-8 border-r border-b border-l border-grey-light border-t bg-white rounded"
+      class="mt-8 max-w-md w-full flex flex-row items-start p-10 border-r border-b border-l border-grey-light border-t bg-white rounded"
     >
-      <img
-        class="w-10 h-10 rounded-full mr-4"
-        :src="item.card_svg"
-      >
-      <div class="flex flex-col justify-start items-start content-start text-left">
+      <div class="w-1/6 mr-5">
+        <img class :src="item.card_img">
+      </div>
+      <div class="w-5/6 flex flex-col justify-center items-start text-left">
         <h3 class="font-semibold learing-tight text-xl">{{ item.card_title }}</h3>
-        <p
-          class="mt-2 font-regular leading-normal text-base opacity-75"
-        >{{ item.card_paragraph }}</p>
+        <p class="mt-2 font-regular leading-normal text-base opacity-75">{{ item.card_paragraph }}</p>
       </div>
     </div>
   </div>
