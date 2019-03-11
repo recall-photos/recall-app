e97c38464aa95d1227ac946cf5187991e95bce45
Jack Veiga <hello@jackveiga.com>
2019-03-11 22:31:50 +0800

    Changes to eslint and add prettier to project

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,17 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
+    'prettier',
+    'prettier/vue',
+  ],
+  plugins: [
+    'prettier',
+    'vue',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 'error',
   },
   parserOptions: {
     parser: 'babel-eslint',
